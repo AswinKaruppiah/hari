f260ce3283dff91eb7f03ea50c2fada69339bc11
aswinth@dextrlabs.com <89991183+AswinKaruppiah@users.noreply.github.com>
2025-07-21 23:19:27 +0530

    Base Setup

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <section className="demo-hero-section">
+      <h1 className="text-9xl text-center">Travels</h1>
+    </section>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        "2md": "900px",
+        xs: "480px",
+        "3xl": "1800px",
+      },
+    },
+  },
+  plugins: [],
+};
